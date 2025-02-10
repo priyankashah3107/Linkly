fcfa539436c6e9ba11dcc1f4fd5d70d071c46e9b
priyankashah3107 <priyankashah3107@gmail.com>
2025-02-10 23:02:12 +0530

    footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="text-center py-4 text-lg font-semibold">
+      
+        <div className="flex flex-row gap-6 justify-center">
+        <p className="flex felx-row gap-4">
+        Created by <a href="https://github.com/priyankashah3107" target="_blank" rel="noopener noreferrer" className="text-blue-400">Priyanka Shah</a> | 
+        <a href="https://github.com/priyankashah3107" target="_blank" rel="noopener noreferrer" className="text-blue-400">GitHub</a> 
+        <a href="https://x.com/priyankashah317" target="_blank" rel="noopener noreferrer" className="text-blue-400">Twitter</a>
+        </p>
+        </div>
+    
+    </footer>
+  )
+}
+
+export default Footer
